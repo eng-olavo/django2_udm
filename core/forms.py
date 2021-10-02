0a4e9a3658f5513c74feb37623026bcb18af5b5e
Olavo-DIO <olavo.melo.br.DIO@gmail.com>
2021-10-02 12:26:11 -0300

    primeiro commmit até a aula-41 model Produto

--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.core.mail.message import EmailMessage
 
 class ContatoForm(forms.Form):
-    nome = forms.CharField(label='None', max_length=100)
+    nome = forms.CharField(label='Nome', max_length=100)
     email = forms.EmailField(label='E-mail', max_length=100)
     assunto = forms.CharField(label='Assunto', max_length=100)
     mensagem = forms.CharField(label='Mensagem', max_length=100)
